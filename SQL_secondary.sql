1f58a45aa2ec50769ccc10611b72f594bac8aabe
RadovanJankovic <146125704+RadovanJankovic@users.noreply.github.com>
2023-11-19 20:44:00 +0100

    Delete SQL_secondary.sql

--- SQL_secondary.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	e.country,
-	e.GDP,
-	e.gini,
-	e.`year`
-FROM economies e 
-CROSS JOIN countries c ON e.country = c.country AND e.YEAR >= 2006 AND e.YEAR <= 2018
-WHERE c.continent = 'Europe'
-ORDER BY e.YEAR;
