f21ccadb70250cc6cbc50e871cbb70b4e67e731a
RadovanJankovic <146125704+RadovanJankovic@users.noreply.github.com>
2023-11-26 17:34:01 +0100

    Update Otazka_5.sql

--- Otazka_5.sql
+++ Otazka_5.sql
@@ -1,7 +1,7 @@
 SELECT 
 	t1.rok,
-	AVG (t1.priemer_cena_zbozi) AS "priemerne_ceny",
-	AVG (t1.priemer_mzdy_rok) AS "priemerna_mzda",
+	ROUND (AVG (t1.priemer_cena_zbozi),2) AS "priemerne_ceny",
+	ROUND (AVG (t1.priemer_mzdy_rok),0) AS "priemerna_mzda",
 	t2.GDP AS "HDP"
 FROM t_radovan_jankovic_project_sql_primary_final t1
 JOIN t_radovan_jankovic_project_sql_seconday_final t2 ON t1.rok=t2.`year` 
