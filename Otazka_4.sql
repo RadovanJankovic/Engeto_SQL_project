943b978eccab6af62ba31833ee490e40b9063383
RadovanJankovic <146125704+RadovanJankovic@users.noreply.github.com>
2023-11-26 17:20:26 +0100

    Update Otazka_4.sql

--- Otazka_4.sql
+++ Otazka_4.sql
@@ -3,7 +3,7 @@ WITH zmena_mzdy AS (
 		t1.rok AS rok,
 		t1.priemer_mzdy_rok AS mzda_minuly_rok,
 		t2.priemer_mzdy_rok AS mzda_aktualne,
-		ROUND (((AVG (t1.priemer_mzdy_rok)-(AVG (t2.priemer_mzdy_rok)))/AVG(t2.priemer_mzdy_rok))*100,2) AS "medziroc_zmena_mzdy"
+		ROUND ((((AVG (t1.priemer_mzdy_rok))-(AVG (t2.priemer_mzdy_rok)))/AVG(t2.priemer_mzdy_rok))*100,2) AS "medziroc_zmena_mzdy"
 	FROM t_radovan_jankovic_project_sql_primary_final t1
 	JOIN t_radovan_jankovic_project_sql_primary_final t2 ON t1.rok = t2.rok+1
 	GROUP BY t1.rok 
@@ -13,7 +13,7 @@ WITH zmena_mzdy AS (
 		t1.rok AS rok,
 		t1.priemer_cena_zbozi AS ceny_minuly_rok,
 		t2.priemer_cena_zbozi AS ceny_aktualne,
-		ROUND (((AVG(t1.priemer_cena_zbozi)-(AVG(t2.priemer_cena_zbozi)))/AVG(t2.priemer_cena_zbozi))*100,2) AS "medziroc_zmena_ceny"
+		ROUND ((((AVG(t1.priemer_cena_zbozi))-(AVG(t2.priemer_cena_zbozi)))/AVG(t2.priemer_cena_zbozi))*100,2) AS "medziroc_zmena_ceny"
 	FROM t_radovan_jankovic_project_sql_primary_final t1
 	JOIN t_radovan_jankovic_project_sql_primary_final t2 ON t1.rok = t2.rok+1
 	GROUP BY t1.rok
